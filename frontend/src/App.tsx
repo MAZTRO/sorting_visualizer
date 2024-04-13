7fa1514d45ab7c51bd681681c9afcfc7bad37bb9
MAZTRO <design.mallet@gmail.com>
2024-04-13 15:47:51 -0500

    Adding base folder and configurations

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <main>
+      <h1>Sorting Visualizer app</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5000,
+    host: 'localhost'
+  },
+  preview: {
+    port: 10080
+  },
+  build: {
+    outDir: './build/dist',
+    minify: 'terser',
+    emptyOutDir: true
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      assets: '/src/assets',
+      pages: '/src/pages',
+      utils: '/src/utils'
+    }
+  }
+})
